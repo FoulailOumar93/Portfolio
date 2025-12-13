aa7ff1246503b7f1641c4cedeee5cf96cbdbe0f1
Foulail Oumar <100520938+FoulailOumar93@users.noreply.github.com>
2025-12-13 13:16:35 +0100

    Update menu.js

--- JS/menu.js
+++ JS/menu.js
@@ -1,4 +1,3 @@
-// ✅ Menu généré entièrement en JavaScript
 document.getElementById("menu-container").innerHTML = `
   <header>
     <input type="checkbox" id="burger-toggle" />
@@ -8,19 +7,18 @@ document.getElementById("menu-container").innerHTML = `
 
     <nav>
       <ul>
-        <li><a href="/Portfolio/HTML/Accueil.html" data-i18n="menu_home">Accueil</a></li>
-        <li><a href="/Portfolio/HTML/Profil.html" data-i18n="menu_profile">Profil</a></li>
-        <li><a href="/Portfolio/HTML/BTS SIO.html" data-i18n="menu_bts">BTS SIO</a></li>
-        <li><a href="/Portfolio/HTML/OClock.html" data-i18n="menu_oclock">O'Clock</a></li>
-        <li><a href="/Portfolio/HTML/Projets.html" data-i18n="menu_projects">Projets</a></li>
-        <li><a href="/Portfolio/HTML/Stage.html" data-i18n="menu_stage">Stage</a></li>
-        <li><a href="/Portfolio/HTML/Veille Technologique.html" data-i18n="menu_veille">Veille Technologique</a></li>
-        <li><a href="/Portfolio/HTML/Certification.html" data-i18n="menu_certif">Certification</a></li>
-        <li><a href="/Portfolio/HTML/Contact.html" data-i18n="menu_contact">Contact</a></li>
+        <li><a href="../HTML/Accueil.html" data-i18n="menu_home">Accueil</a></li>
+        <li><a href="../HTML/Profil.html" data-i18n="menu_profile">Profil</a></li>
+        <li><a href="../HTML/BTS SIO.html" data-i18n="menu_bts">BTS SIO</a></li>
+        <li><a href="../HTML/OClock.html" data-i18n="menu_oclock">O'Clock</a></li>
+        <li><a href="../HTML/Projets.html" data-i18n="menu_projects">Projets</a></li>
+        <li><a href="../HTML/Stage.html" data-i18n="menu_stage">Stage</a></li>
+        <li><a href="../HTML/Veille Technologique.html" data-i18n="menu_veille">Veille Technologique</a></li>
+        <li><a href="../HTML/Certification.html" data-i18n="menu_certif">Certification</a></li>
+        <li><a href="../HTML/Contact.html" data-i18n="menu_contact">Contact</a></li>
       </ul>
     </nav>
 
-    <!-- 🔥 Bouton multi-langue -->
     <button id="lang-switch" class="lang-btn">FR</button>
   </header>
 `;
